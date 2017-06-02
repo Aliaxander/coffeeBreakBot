6232e63b92d0eadb7f96f34ebfe73808b9445711
aliaxander <admin@oxcpa.ru>
2017-06-02 13:23:47 +0300

    debug

--- OxApp/controllers/WebhookController.php
+++ OxApp/controllers/WebhookController.php
@@ -35,7 +35,7 @@ class WebhookController extends App
         $lang = Config::$lang['ru'];
         $token = Bots::find(['id' => $botId])->rows[0]->api;
         $telegram = new Api($token);
-        print_r($telegram->setWebhook(['url'=>'https://coffe.oxgroup.media']));
+        print_r($telegram->setWebhook(['url'=>'https://coffe.ebot.biz/webhook']));
         /*
      $message = $telegram->getWebhookUpdate();
      print_r($message);
