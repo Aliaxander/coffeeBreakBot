dfa08db9b2e31c3c09932299c02ecc455e198a0a
aliaxander <admin@oxcpa.ru>
2017-06-02 14:12:13 +0300

    add menu

--- OxApp/controllers/WebhookController.php
+++ OxApp/controllers/WebhookController.php
@@ -74,7 +74,7 @@ class WebhookController extends App
             'chat_id' => $chatId,
             'text' => 'Set coffee:',
             'reply_markup' => $reply_markup,
-           // 'reply_to_message_id' => $chatId
+            'reply_to_message_id' =>  $message->getMessage()->get("message_id")
         ]);
         
         //
