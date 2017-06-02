b7f43d56bbc078f8e20dc075665ab5285c9bc83d
aliaxander <admin@oxcpa.ru>
2017-06-02 14:04:44 +0300

    add menu

--- OxApp/controllers/WebhookController.php
+++ OxApp/controllers/WebhookController.php
@@ -35,7 +35,7 @@ class WebhookController extends App
         $lang = Config::$lang['ru'];
         $token = Bots::find(['id' => $botId])->rows[0]->api;
         $telegram = new Api($token);
-        print_r($telegram->setWebhook(['url'=>'https://coffe.ebot.biz/webhook']));
+        //print_r($telegram->setWebhook(['url'=>'https://coffe.ebot.biz/webhook']));
        //$telegram->removeWebhook();
         
         $message = $telegram->getWebhookUpdate();
@@ -71,7 +71,7 @@ class WebhookController extends App
             'chat_id' => $chatId,
             'text' => 'Set coffee:',
             'reply_markup' => $reply_markup,
-           // 'reply_to_message_id' => $chatId
+            'reply_to_message_id' => $message->getMessage()->getFrom()->getId()
         ]);
         
         //
